36a732f73c8713d7b62a1afd30b75b085e5f657a
oldcryptogeek <oldcryptogeek@github.com>
2019-10-06 15:06:42 +0000

    add view balance

---
+++ view_balance.py
@@ -0,0 +1,12 @@
+import os
+
+
+class BalanceViewer:
+    def __init__(self, account_key, node):
+        self.account_key = account_key
+        self.node = node
+        os.system('jcli rest v0 account get ' + self.account_key +
+                  ' -h ' + self.node + '/api > balance.json')
+        with open('balance.json', 'r') as f:
+            print(f.read())
+        os.remove('balance.json')
