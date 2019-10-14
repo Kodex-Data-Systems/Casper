1e568a5f5f9c9d86198ceb56eab678a73d6fe004
oldcryptogeek <37357922+oldcryptogeek@users.noreply.github.com>
2019-10-13 20:30:53 -0600

    Update show_blockchain_size.py

--- show_blockchain_size.py
+++ show_blockchain_size.py
@@ -7,7 +7,7 @@ class ShowBlockchainSize:
     def __init__(self):
         try:
             print()
-            size = subprocess.check_output('ls -h ' + STORAGE, shell=True).decode()
+            size = subprocess.check_output('ls -lrth ' + STORAGE, shell=True).decode()
             print(size)
             
         except subprocess.CalledProcessError:
