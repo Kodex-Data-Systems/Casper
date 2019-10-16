3e0c0c920c29c5193f2d5db1cadc10c6ebddd079
oldcryptogeek <37357922+oldcryptogeek@users.noreply.github.com>
2019-10-15 19:12:32 -0600

    Create show_leaders_logs.py

---
+++ show_leaders_logs.py
@@ -0,0 +1,14 @@
+import subprocess
+
+from config import NODE
+
+
+class LeadersLogs:
+    def __init__(self):
+        try:
+            print()
+            log = subprocess.check_output('jcli rest v0 leaders logs get -h ' + NODE + '/api', shell=True).decode()
+            print(log)
+            
+        except subprocess.CalledProcessError:
+            print('Unable to connect')
